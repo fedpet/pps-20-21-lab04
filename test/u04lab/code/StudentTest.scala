8fe331a10ab3744f67896cbe8372a99e7d47d613
fedpet <federico.pettinari2@studio.unibo.it>
2021-03-22 18:58:37 +0100

    SameTeacher test

--- test/u04lab/code/StudentTest.scala
+++ test/u04lab/code/StudentTest.scala
@@ -1,15 +1,17 @@
 package u04lab.code
 
 import org.junit.jupiter.api.Assertions.{assertEquals, assertFalse, assertTrue}
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.{Assertions, Test}
 import u04lab.code.Lists.List.Cons
 import u04lab.code.Lists.List.Nil
+import u04lab.code.Lists.List
 
 class StudentTest {
   @Test def testStudent() {
     assertEquals("mario", Student("mario").name)
     assertEquals(2000, Student("mario", 2000).year)
   }
+
   @Test def testStudentEnrolling() {
     val s1 = Student("mario",2015)
     val c1 = Course("PPS","Viroli")
@@ -18,5 +20,12 @@ class StudentTest {
     assertEquals(Cons("PCD", Cons("PPS", Nil())), s1.courses)
     assertTrue(s1.hasTeacher("Ricci"))
     assertFalse(s1.hasTeacher("Unknown"))
+  }
+
+  @Test def testSameTeacher() {
+    assertEquals(scala.Option("Viroli"), sameTeacher.unapply(List(Course("V1","Viroli"), Course("V2","Viroli"))))
+    assertEquals(scala.Option.empty, sameTeacher.unapply(List(Course("V1","Viroli"), Course("V2","Viroli"), Course("R1","Ricci"))))
+    assertEquals(scala.Option("Viroli"), sameTeacher.unapply(List(Course("V1","Viroli"))))
+    assertEquals(scala.Option.empty, sameTeacher.unapply(List()))
   }
 }
